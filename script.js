4b6bf10353d0caf2bcb29e863c20a220ef386078
Gafuroff-prog <gafuroffgaming@gmail.com>
2024-11-15 14:33:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstname = prompt('Ismingizni kiriting.');
+alert(`Sizning ismingiz ${firstname}`);
+let letter = prompt('Bitta harf kiriting:');
+if(firstname<letter){
+    alert(`ismingizda ${letter} harfi bor`);
+}
+else{
+    alert(`ismingizda ${letter} harfi yoq`);
+}    
